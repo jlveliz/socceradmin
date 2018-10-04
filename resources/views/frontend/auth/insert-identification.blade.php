70bd1e716f220712bd95dfc656a8fa49298bb3ff
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2018-10-03 22:55:37 -0500

    cambios

--- resources/views/frontend/auth/insert-identification.blade.php
+++ resources/views/frontend/auth/insert-identification.blade.php
@@ -11,14 +11,14 @@
  <!-- Main wrapper  -->
             <div class="container h-100">
                 <div class="row h-100 justify-content-center align-items-center">
-                    <div class="col-12 card">
+                    <div class="col-lg-5 col-md-8 col-xs-12 col-sm-12 card">
                             <div class="login-form">
                                 <h3 class="text-center">Registro para Inicio de Clases Regulares y Demostrativas</h3>
 
                                 <form class="needs-validation @if($errors->has('num_identification')) was-validated @endif" action="{{ route('register-verify') }}" method="post" novalidate>
                                     {{ csrf_field() }}
                                     <div class="row">
-                                        <div class="col-sm-12 col-lg-12 form-group">
+                                        <div class="col-sm-12 col-lg-12 col-xs-12 form-group">
                                             <label for="num_identification">Cédula del Representante</label>
                                             <input id="num_identification" name="num_identification" type="text" class="form-control" autofocus autocomplete="false" required pattern="[0-9]+" minlength="10" maxlength="10" value="{{ old('num_identification')}}" @if($errors->has('num_identification')) style="border-color: ##dc3545" @endif>
                                             <div class="invalid-feedback">Ingrese una cédula.</div>
