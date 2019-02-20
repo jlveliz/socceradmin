24704d428e68c17a268d863a951c7f7d53667137
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-02-20 15:26:18 -0500

    termina login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 
-<div class="col-md-8 py-5">
+<div class="col-md-9 py-5">
     <div class="p-4">
         <div class="auth-logo text-center mb-4">
             <img src="{{asset('assets/images/logo.png')}}" alt="">
@@ -12,18 +12,18 @@
         <h1 class="mb-3 text-18">Ingreso</h1>
         <form class="login-form" action="{{ route('login') }}" method="POST">
             {{ csrf_field() }}
-            <div class="form-group {{ $errors->has('email') ? ' is-invalid' : '' }}">
-                <label for="email" class="text-uppercase">Email</label>
-                <input type="text" class="form-control" id="email" placeholder="" name="email" value="{{ old('email') }}" autofocus="">
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control form-control-rounded {{ $errors->has('email') ? ' is-invalid' : '' }}" id="email" placeholder="" name="email" value="{{ old('email') }}" autofocus="">
                 @if ($errors->has('email'))
                     <span class="invalid-feedback animated fadeInDown">
                         <strong>{{ $errors->first('email') }}</strong>
                     </span>
                 @endif
             </div>
-            <div class="form-group {{ $errors->has('password') ? ' is-invalid' : '' }}">
-                <label for="password" class="text-uppercase">Contraseña</label>
-                <input type="password" id="password" name="password" class="form-control">
+            <div class="form-group">
+                <label for="password">Contraseña</label>
+                <input type="password" id="password" name="password" class="form-control form-control-rounded {{ $errors->has('password') ? ' is-invalid' : '' }}">
                 @if ($errors->has('password'))
                     <span class="invalid-feedback animated fadeInDown">
                         <strong>{{ $errors->first('password') }}</strong>
@@ -35,7 +35,7 @@
                     <input type="checkbox" class="form-check-input" name="remember" {{ old('remember') ? 'checked' : '' }}>
                     <small>Recordarme</small>
                 </label>
-                <button type="submit" class="btn btn-login float-right">
+                <button type="submit" class="btn btn-rounded btn-primary btn-block mt-2">
                     <span class="signingin hidden"><span class="voyager-refresh"></span> Ingresando...</span>
                     <span class="signin">Ingresar</span>
                 </button>
@@ -46,7 +46,7 @@
 @endsection
 
 @section('js')
-<script>
+<script type="text/javascript">
     var btn = document.querySelector('button[type="submit"]');
     var form = document.forms[0];
     var email = document.querySelector('[name="email"]');
