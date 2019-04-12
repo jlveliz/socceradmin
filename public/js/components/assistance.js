121af26fc9d3b088a7a1ca873617c22f2dbd41e9
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-04-12 15:54:54 -0500

    commit

--- public/js/components/assistance.js
+++ public/js/components/assistance.js
@@ -1,69 +1,69 @@
 jQuery(document).ready(function($) {
 
-	$('[data-toggle="tooltip"]').tooltip()
-	
-	$('#field-id').on('change',function(event) {
-		$('#filter-assistance').submit();
-	});
+    $('[data-toggle="tooltip"]').tooltip()
 
-	$('#key-day').on('change',function(event) {
-		$('#filter-assistance').submit();
-	});
+    $('#field-id').on('change', function(event) {
+        $('#filter-assistance').submit();
+    });
 
-	$('#group_id').on('change',function(event) {
-		$('#filter-assistance').submit();
-	});
+    $('#key-day').on('change', function(event) {
+        $('#filter-assistance').submit();
+    });
 
-	$('#group-key').on('change',function(event) {
-		$('#filter-assistance').submit();
-	});
+    $('#group_id').on('change', function(event) {
+        $('#filter-assistance').submit();
+    });
 
-
-	$('.check-assistance').on('click', function(event) {
-		$check = $(event.currentTarget);
-		if (!$check.is(':checked')) {
-			var message = confirm('Está sguro?');
-			if (!message) {
-				$check.prop('checked','checked')
-			} else {
-				$check.closest('.form-check-inline').find('.show-message').removeClass('visible').addClass('invisible')
-				$check.closest('.form-check-inline').find('.comment-hidden').val('');
-			}
-		} else {
-			$("#insertCommentModal").modal('show');
-			$("#insertCommentModal").trigger('pass-check',$check.attr('id'));
-		}
-	});
-
-	$("#insertCommentModal").on('show.bs.modal',function(event){		
-		var $modal = $(event.currentTarget);
-		var showMessageIcon = $(event.relatedTarget);
-		var $check = showMessageIcon.closest('.form-check-inline').find('input[type="checkbox"]');
-		var comment = showMessageIcon.closest('.form-check-inline').find('.comment-hidden').val()
-		if (comment) {
-			$modal.find('input[type=text]').val(comment).attr('autofocus');
-		} else {
-			$modal.find('input[type=text]').val('').attr('autofocus');
-		}
-		$("#insertCommentModal").trigger('pass-check',$check.attr('id'));
-	});
-
-	$("#insertCommentModal").on('pass-check',function(event, param){
-		var $modal = $(event.currentTarget);
-		$modal.find('#button-target').val(param)
-	})
+    $('#group-key').on('change', function(event) {
+        $('#filter-assistance').submit();
+    });
 
 
-	$("#insertCommentModal").on('hide.bs.modal',function(event){
-		var $modal = $(event.currentTarget);
-		var idTargetChecked = $modal.find('#button-target').val();
-		var comment = $modal.find('#comment').val();
-		if (comment && idTargetChecked) {
-			$('#'+idTargetChecked).closest('.form-check-inline').find('.comment-hidden').val(comment);
-			$('#'+idTargetChecked).closest('.form-check-inline').find('.show-message').removeClass('invisible').addClass('visible')
-		}
+    $('.check-assistance').on('click', function(event) {
+        $check = $(event.currentTarget);
+        if (!$check.is(':checked')) {
+            var message = confirm('Está sguro?');
+            if (!message) {
+                $check.prop('checked', 'checked')
+            } else {
+                $check.closest('.form-check-inline').find('.show-message').removeClass('visible').addClass('invisible')
+                $check.closest('.form-check-inline').find('.comment-hidden').val('');
+            }
+        } else {
+            $("#insertCommentModal").modal('show');
+            $("#insertCommentModal").trigger('pass-check', $check.attr('id'));
+        }
+    });
 
-	});
+    $("#insertCommentModal").on('show.bs.modal', function(event) {
+        var $modal = $(event.currentTarget);
+        var showMessageIcon = $(event.relatedTarget);
+        var $check = showMessageIcon.closest('.form-check-inline').find('input[type="checkbox"]');
+        var comment = showMessageIcon.closest('.form-check-inline').find('.comment-hidden').val()
+        if (comment) {
+            $modal.find('input[type=text]').val(comment).attr('autofocus');
+        } else {
+            $modal.find('input[type=text]').val('').attr('autofocus');
+        }
+        $("#insertCommentModal").trigger('pass-check', $check.attr('id'));
+    });
+
+    $("#insertCommentModal").on('pass-check', function(event, param) {
+        var $modal = $(event.currentTarget);
+        $modal.find('#button-target').val(param)
+    })
+
+
+    $("#insertCommentModal").on('hide.bs.modal', function(event) {
+        var $modal = $(event.currentTarget);
+        var idTargetChecked = $modal.find('#button-target').val();
+        var comment = $modal.find('#comment').val();
+        if (comment && idTargetChecked) {
+            $('#' + idTargetChecked).closest('.form-check-inline').find('.comment-hidden').val(comment);
+            $('#' + idTargetChecked).closest('.form-check-inline').find('.show-message').removeClass('invisible').addClass('visible')
+        }
+
+    });
 
 
 
