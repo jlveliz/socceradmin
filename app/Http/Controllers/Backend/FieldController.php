201c3f6a069ca8e9f9ee718445a58839f91a8413
Jorge Veliz Berzosa <jorgeconsalvacion@gmail.com>
2019-07-17 16:40:59 -0500

    add field to json

--- app/Http/Controllers/Backend/FieldController.php
+++ app/Http/Controllers/Backend/FieldController.php
@@ -26,7 +26,7 @@ class FieldController extends Controller
 
     function __construct(FieldRepositoryInterface $field, AgeRangeRepositoryInterface $ageRange, FieldTypeRepositoryInterface $ftype, CoachRepositoryInterface $coach)
     {
-        $this->middleware('auth',['except' => 'getSchedule']);
+        // $this->middleware('auth',['except' => 'getSchedule']);
         $this->field = $field;
         $this->ageRange = $ageRange;
         $this->ftype = $ftype;
@@ -37,7 +37,7 @@ class FieldController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $fields = $this->field->enum();
         return view('backend.field.index',compact('fields'));
@@ -228,5 +228,15 @@ class FieldController extends Controller
         }
 
         return response('no permitido',401);
+    }
+
+
+    /*
+        Show fields Json
+        @return \Illuminate\Http\Response
+    */
+    public function indexJson()
+    {
+        return response($this->field->enum(),200);
     }
 }
